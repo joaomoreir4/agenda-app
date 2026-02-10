35b0aad56451794e600a207d136634ba7bc20c2e
João Moreira <jpgdmr@gmail.com>
2026-02-10 11:37:48 -0300

    fix: ajustes na tela de login, ícones, e concurrently para iniciar o projeto com um comando

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,4 +1,10 @@
 <svg viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg" {{ $attributes }}>
-  <path d="M11.395 44.428C4.557 40.198 0 32.632 0 24 0 10.745 10.745 0 24 0a23.891 23.891 0 0113.997 4.502c-.2 17.907-11.097 33.245-26.602 39.926z" fill="#6875F5"/>
-  <path d="M14.134 45.885A23.914 23.914 0 0024 48c13.255 0 24-10.745 24-24 0-3.516-.756-6.856-2.115-9.866-4.659 15.143-16.608 27.092-31.75 31.751z" fill="#6875F5"/>
+    <rect x="10" y="6" width="28" height="36" rx="3" fill="#6875F5" />
+    
+    <rect x="6" y="12" width="8" height="3" rx="1.5" fill="white" fill-opacity="0.6" />
+    <rect x="6" y="22" width="8" height="3" rx="1.5" fill="white" fill-opacity="0.6" />
+    <rect x="6" y="32" width="8" height="3" rx="1.5" fill="white" fill-opacity="0.6" />
+    
+    <circle cx="24" cy="20" r="5" fill="white" />
+    <path d="M16 34C16 29.5817 19.5817 26 24 26C28.4183 26 32 29.5817 32 34V35H16V34Z" fill="white" />
 </svg>
